f5614b64f5465c94e240395b80c8e219cea723be
Luuk van den Nouweland <luuk@wearebit.com>
2025-06-04 09:53:16 +0200

    added logging output
    Former-commit-id: cde7ce8ef9b3099d6620180df2ff0c3070718ff3

--- backend/chromadb_query.py
+++ backend/chromadb_query.py
@@ -75,12 +75,14 @@ class ChromadbQuery:
         # database_path = ".." + database_path
         print(database_path)
         print("Path exists:", os.path.exists(database_path))
+
+        logger.info(f"Path exists: {os.path.exists(database_path)}")
         print(os.path.abspath(database_path))
         # Print dir
         print(os.listdir(database_path))
         for root, dirs, files in os.walk(database_path):
             print(f"Directory: {root}\nSubdirectories: {dirs}\nFiles: {files}\n")
-
+            logger.info(f"Directory: {root}\nSubdirectories: {dirs}\nFiles: {files}\n")
         print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
         self.collection_name = collection_name
 
