d8e1f847507d5ef24570de3167f8eec9759023db
Luuk van den Nouweland <l.vanden.nouweland@pzh.nl>
2025-10-21 14:46:23 +0200

    added check if linux

--- backend/data_scraping/overijssel_scraper.py
+++ backend/data_scraping/overijssel_scraper.py
@@ -10,6 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import platform
 from urllib.parse import urlparse, unquote
 import zipfile
 import tempfile
@@ -73,7 +74,8 @@ class Scraper:
         options.add_argument("--window-size=1920,1080")
 
         # Use binary location to point to installed Chrome
-        options.binary_location = "/usr/bin/google-chrome"
+        if platform.system() == "Linux":
+            options.binary_location = "/usr/bin/google-chrome"
 
         # Download and get path to chromedriver
         driver_path = ChromeDriverManager().install()
