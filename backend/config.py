da595f086ee50f7028a081aef84e0ab9c9c25174
Luuk van den Nouweland <l.vanden.nouweland@pzh.nl>
2025-10-22 16:37:34 +0200

    added variable to config

--- backend/config.py
+++ backend/config.py
@@ -25,3 +25,6 @@ JSON_FOLDER = os.path.join(BASE_DIR, "json")
 # ChromaDB configuration
 CHROMA_DB_DIR = os.path.join(BASE_DIR, "chromadb")
 CHROMA_COLLECTION_NAME = "pdf_chunks"
+
+# Path to which succesfully crawled and processed URLs will be written
+URLS_WRITE_LOCATION = os.path.join(BASE_DIR, "URLs.txt")
